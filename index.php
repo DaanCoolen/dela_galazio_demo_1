bf3f3610a358d74ef32dc8eb0d1acd0dbd085796
Daan Coolen <61143444+DaanCoolen@users.noreply.github.com>
2023-05-09 20:58:13 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$helloWorld = "Hello World from Galazio for DevOps demo"
+?>
+<html>
+    <head>
+        <title><?php echo $helloWorld ?></title>
+    </head>
+    <body>
+        <h1><?php echo $helloWorld ?></h1>
+    </body>
+</html>
